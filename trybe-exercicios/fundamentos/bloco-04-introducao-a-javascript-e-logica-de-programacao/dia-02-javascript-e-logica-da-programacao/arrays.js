027929b0d5b937f30b00c66ea01c0dd55b938ef0
Karen <karen.fernandes.cano@gmail.com>
2021-12-05 15:21:11 -0300

    Exercícios dia 4.2

---
+++ trybe-exercicios/fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-e-logica-da-programacao/arrays.js
@@ -0,0 +1,22 @@
+//exercicio 1
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+//exercicio 2
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu[menu.length -2];
+
+// console.log(indexOfPortfolio);
+
+//exercicio 3
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push = 'Contato';
+
+// console.log(menu);
+
+
